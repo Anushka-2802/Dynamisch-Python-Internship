d6570f0ee1b3f1cd676e8f9a1dd0a077a4f789cd
Anushka Gujar <anushkagujar39@gmail.com>
2026-07-02 12:11:44 +0530

    Updated QSeries3 q7 program

---
+++ QSeries3/q7_logging_transaction.py
@@ -0,0 +1,45 @@
+import logging
+
+logging.basicConfig(
+    filename="transaction.log",
+    level=logging.ERROR,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+# Custom Exception
+class InvalidTransactionError(Exception):
+    """Custom Exception"""
+    pass
+
+
+def payment():
+    """
+    Processes a payment transaction.
+    Raises:
+        InvalidTransactionError:
+            - If the entered amount is negative
+            - If the entered amount exceeds account balance
+        Logs:
+        Error details with timestamp and transaction ID
+        into the transaction.log file.
+    """
+    transaction_id = input("Enter Transaction Id: ")
+    amount = int(input("Enter Amount: "))
+
+    balance = 10000
+
+    try:
+        if amount < 0:
+            raise InvalidTransactionError("Invalid Amount")
+
+        if amount > balance:
+            raise InvalidTransactionError("Insufficient Balance")
+
+        print(f"Transaction {transaction_id}: {amount} Successful!!!!")
+
+    except InvalidTransactionError as e:
+        logging.error(f"Transaction Id: {transaction_id} - {e}")
+        print("Transaction Failed")
+
+payment()
+print(payment.__doc__) #print doc string 
